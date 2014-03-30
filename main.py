3139cd603dbe0ecdda1a7166767795ae7f5101d4
bedekelly <bedekelly97@gmail.com>
2014-03-30 12:38:39 +0100

    Added proof of concept for threaded value-updates.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-|================================|
-| Gold Clicker -- Text Edition   |
-|================================|
+|=====================================|
+| Gold Tapper -- Workin' all day long |
+|=====================================|
 
 Copyright © 2014 Bede Kelly <bedekelly@mail.com>
 This work is free. You can redistribute it and/or modify it under the
@@ -15,26 +15,31 @@ as published by Sam Hocevar. See the COPYING file for more details.
 import curses
 import curses.panel
 import time
+import threading
+from threading import Thread
 from collections import namedtuple
 GOLD_PIECES = 0
 TAPPED_ITEMS = 0
+GOLD_INCREMENT_VALUE = 1
+GOLD_UPDATE_INTERVAL = 1  # second(s)
+
 
 class Credits:
-    def __init__(self, screen):
+    def __init__(self, stdscr):
         try:
             self.text = open("credits", "r").readlines()
         except FileNotFoundError:
-            file_not_found(screen)
+            file_not_found(stdscr)
         self.indent = 5  # columns
         self.start_line = 5  # rows
 
-    def display(self, screen):
-        screen.clear()
+    def display(self, stdscr):
+        stdscr.clear()
         for index, line in enumerate(self.text):
-            screen.addstr(index + self.start_line, self.indent, line)
-        screen.border(0)
-        screen.refresh()
-        screen.getkey()
+            stdscr.addstr(index + self.start_line, self.indent, line)
+        stdscr.border(0)
+        stdscr.refresh()
+        stdscr.getkey()
 
 
 class Menu:
@@ -45,14 +50,15 @@ class Menu:
         self.headline_ypos = CENTER.y_coordinate - 3
         self.indent = indent
 
-    def print_all(self, screen):
-        screen.addstr(self.headline_ypos, self.headline_xpos, self.headline)
+    def print_all(self, stdscr, CENTER):
+        stdscr.addstr(self.headline_ypos, self.headline_xpos, self.headline)
         for index, item in enumerate(self.list_items):
-            screen.addstr(self.headline_ypos + index + 2, self.indent,
+            item_hpos = get_horizontal_position(item.text, CENTER)
+            stdscr.addstr(self.headline_ypos + index + 2, item_hpos,
                           item.text, curses.A_REVERSE if item.selected
                           else curses.A_NORMAL)
-        screen.border(0)
-        screen.refresh()
+        stdscr.border(0)
+        stdscr.refresh()
 
     class MenuItem:
         def __init__(self, text, func=None):
@@ -63,110 +69,165 @@ class Menu:
             self.selected = True
         def deselect(self):
             self.selected = False
-        def __call__(self, screen, CENTER):
+        def __call__(self, stdscr, CENTER):
             if self.func is not None:
-                self.func(screen, CENTER)
+                self.func(stdscr, CENTER)
         def __str__(self):
             return self.text
 
 
-def file_not_found(screen):
-    screen.clear()
-    screen.addstr(5, 5, "A critical file is missing. Please reinstall the "
-                        "game.")
-    screen.getkey()
+class TaskThread:
+    """Thread that executes a task every n seconds. Thanks to 
+    Itamar Shtull-Trauring"""
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self._finished = threading.Event()
+        self._interval = GOLD_UPDATE_INTERVAL
+
+    def set_interval(self, interval):
+        self._interval = interval
+    def shutdown(self):
+        self._finished.set()
+    def run(self, param):
+        while True:
+            if self._finished.isSet():
+                return
+            self.task(param)
+            self._finished.wait(self._interval)
+    def task(self):
+        """The task performed by this thread -- override in subclasses."""
+        pass
+
+
+class GoldUpdater(TaskThread):
+    def task(self, gold_panel):
+        if not gold_panel.hidden():
+            gold_window = gold_panel.window()
+            gold_window.clear()
+            gold_window.addstr(2, 3, "Gold pieces: {}".format(GOLD_PIECES))
+            gold_window.refresh() 
+
+
+def file_not_found(stdscr):
+    stdscr.clear()
+    stdscr.addstr(5, 5, "A critical file is missing. "
+                        "Please reinstall the game.")
+    stdscr.getkey()
     quit()
 
 
-def print_credits(screen, _):
-    credits = Credits(screen)
-    credits.display(screen)
+def print_credits(stdscr, _):
+    credits = Credits(stdscr)
+    credits.display(stdscr)
 
 
-def get_dimensions(item, screen):
+def get_dimensions(item, stdscr):
     if item == "gold":
         lines = 5
         columns = 20
-        _, H_MAX = screen.getmaxyx()
+        _, H_MAX = stdscr.getmaxyx()
         y = 1
         x = H_MAX - columns - 2
 
-    elif item == "stats":
-        lines = 10
-        columns = 20
+    elif item == "tapped":
+        lines = 5
+        columns = 21
         y = 1
-        x = 1
+        x = 2
 
     return lines, columns, y, x
+
 
 def display_gold_window(gold_window, gold_panel):
     gold_panel.show()
     gold_window.bkgd(' ', curses.color_pair(2))
     gold_window.clear()
-    gold_window.addstr(1, 1, "Gold pieces: {}".format(GOLD_PIECES))
+    gold_window.addstr(2, 3, "Gold pieces: {}".format(GOLD_PIECES))
     gold_window.border(0)
     gold_window.refresh()
 
 
-def display_stats_window(stats_window, stats_panel):
-    stats_panel.show()
-    stats_window.bkgd(' ', curses.color_pair(1))
-    stats_window.clear()
-    stats_window.addstr(1, 1, "Total tapped: {}".format(TAPPED_ITEMS))
-    stats_window.border(0)
-    stats_window.refresh()
+def display_tapped_window(tapped_window, tapped_panel):
+    tapped_panel.show()
+    tapped_window.bkgd(' ', curses.color_pair(1))
+    tapped_window.clear()
+    tapped_window.addstr(2, 3, "Total tapped: {}".format(TAPPED_ITEMS))
+    tapped_window.border(0)
+    tapped_window.refresh()
 
 
-def play_game(screen, CENTER):
-    screen.clear()
-    gold_window = curses.newwin(*get_dimensions("gold", screen))
+def display_hud(stdscr, stdscr_panel, tapped_panel, gold_panel,
+display_gold, display_tapped):
+    gold_window = gold_panel.window()
+    tapped_window = tapped_panel.window()
+    if display_gold:
+        display_gold_window(gold_window, gold_panel)
+    else:
+        gold_panel.hide()
+    if display_tapped:
+        display_tapped_window(tapped_window, tapped_panel)
+    else:
+        tapped_panel.hide()
+    curses.panel.update_panels()
+    stdscr.refresh()
+
+
+# def update_gold_window(gold_window):
+#     while True:
+#         if not KILL_GOLD_UPDATE:
+#             time.sleep(GOLD_UPDATE_INTERVAL)
+#             gold_window.clear()
+#             gold_window.addstr(2, 3, "Gold pieces: {}".format(GOLD_PIECES))
+#             gold_window.refresh()
+#         else:
+#             return
+
+
+def play_game(stdscr, CENTER):
+    # Setup main screen
+    stdscr.clear()
+    stdscr.border(0)
+    # Setup screen items
+    gold_window = curses.newwin(*get_dimensions("gold", stdscr))
     gold_panel = curses.panel.new_panel(gold_window)
-
-    stats_window = curses.newwin(*get_dimensions("stats", screen))
-    stats_panel = curses.panel.new_panel(stats_window)
-
-    screen_panel = curses.panel.new_panel(screen)
+    tapped_window = curses.newwin(*get_dimensions("tapped", stdscr))
+    tapped_panel = curses.panel.new_panel(tapped_window)
+    stdscr_panel = curses.panel.new_panel(stdscr)
+    # Panels hidden by default
     display_gold = False
-    display_stats = False
-    while True:
-        screen_panel.bottom()
-        if display_gold:
-            display_gold_window(gold_window, gold_panel)
-            # gold_panel.show()
-            # gold_window.border(0)
-            # gold_window.addstr(0,0,"Test gold window")
-            # gold_window.refresh()
-        else:
-            gold_panel.hide()
+    display_tapped = False
 
-        if display_stats:
-            display_stats_window(stats_window, stats_panel)
-            # stats_panel.show()
-            # stats_window.border(0)
-            # stats_window.addstr(0,0,"Test stats window")
-            # stats_window.refresh()
-        else:
-            stats_panel.hide()
-        curses.panel.update_panels()
-        screen.refresh()
-        key = screen.getkey()
+    # Setup gold to update every GOLD_UPDATE_INTERVAL seconds
+    gold_window_updater = GoldUpdater()
+    gold_window_updater.run(gold_panel)
+    # Main input loop
+    while True:
+        hud_data = (stdscr_panel, tapped_panel, gold_panel,
+                        display_gold, display_tapped)
+        display_hud(stdscr, *hud_data)
+
+
+        key = stdscr.getkey()
         if key == "g":
             display_gold = not display_gold  # toggle
-        elif key == "s":
-            display_stats = not display_stats  # toggle
+        elif key == "t":
+            display_tapped = not display_tapped  # toggle
+        elif key == "p":
+            GOLD_PIECES += 1
         elif key == "q":
+            KILL_GOLD_UPDATE = True
             return
 
 
 def get_horizontal_position(text, CENTER):
-    # Halfway across the screen, minus half the length of the text
+    # Halfway across the stdscr, minus half the length of the text
     return CENTER.x_coordinate - len(text) // 2
 
 
-def get_pos_consts(screen):
+def get_pos_consts(stdscr):
     """Make positional constants"""
     # Get maximum vertical and horizontal coords
-    V_MAX, H_MAX = screen.getmaxyx()
+    V_MAX, H_MAX = stdscr.getmaxyx()
     # Get halfway coords
     V_CENTER, H_CENTER = int(V_MAX // 2), int(H_MAX // 2)
     Coords = namedtuple("Center", ["y_coordinate", "x_coordinate"])
@@ -174,14 +235,15 @@ def get_pos_consts(screen):
     return CENTER
 
 
-def startup_menu(screen, CENTER):
-    headline = "Cookie Clicker -- The REAL Text Version"
+def startup_menu(stdscr, CENTER):
+    headline = "Gold Tapper --- Mining for Shineys"
 
     # Generate menu options
     menu = Menu(headline, CENTER, indent=4)
     menu.playgame = menu.MenuItem("Play Game", func=play_game)
     menu.credits = menu.MenuItem("Credits", func=print_credits)
-    menu.list_items = [menu.playgame, menu.credits]
+    menu.exit = menu.MenuItem("Exit", func=quit)
+    menu.list_items = [menu.playgame, menu.credits, menu.exit]
 
     # Select initial values
     current_item = 0
@@ -189,9 +251,9 @@ def startup_menu(screen, CENTER):
 
     # Main menu loop
     while True:
-        screen.clear()
-        menu.print_all(screen)
-        given_key = screen.getkey()  # Get key input
+        stdscr.clear()
+        menu.print_all(stdscr, CENTER)
+        given_key = stdscr.getkey()  # Get key input
         if given_key == "q":
             break  # Quit
         elif given_key == "KEY_DOWN":
@@ -212,7 +274,7 @@ def startup_menu(screen, CENTER):
     quit()
 
 
-def setup_screen(screen):
+def setup_stdscr(stdscr):
     # Setup a window object, misc
     curses.initscr()
     curses.start_color()
@@ -220,29 +282,34 @@ def setup_screen(screen):
     curses.cbreak()  # No need for [Return]
     curses.noecho()  # Stop keys being printed
     curses.curs_set(0)  # Invisible cursor
-    screen.keypad(True)
-    screen.clear()
-    screen.border(0)
+    stdscr.keypad(True)
+    stdscr.clear()
+    stdscr.border(0)
 
     # Setup color pairs
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
     # Setup background
-    screen.bkgd(' ', curses.color_pair(1))
+    stdscr.bkgd(' ', curses.color_pair(1))
 
     # Setup constants
-    CENTER = get_pos_consts(screen)
-    return screen, CENTER
+    CENTER = get_pos_consts(stdscr)
+    return stdscr, CENTER
 
-def main(screen):
-    screen, CENTER = setup_screen(screen)
+
+def main(stdscr):
+    stdscr, CENTER = setup_stdscr(stdscr)
     # Execute
     while True:
-        choice = startup_menu(screen, CENTER)
+        choice = startup_menu(stdscr, CENTER)
         if choice.func is not None:
-            choice(screen, CENTER)
-
+            if choice.func != quit:
+                choice(stdscr, CENTER)
+            else:
+                quit()
+        else:
+            quit()
 
 if __name__ == "__main__":
     curses.wrapper(main)
---
+++ threading_proof_of_concept.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+
+import curses
+import curses.panel
+from time import sleep
+from threading import Thread
+RUN_UPDATE_WINDOW_THREAD = False
+RUN_UPDATE_NUMBER_THREAD = False
+COUNT_PER_MILLISECOND = 1
+COUNT = 0
+
+
+def number_window_updater():
+    with open("log", "w") as logfile:
+        number_window = curses.newwin(5,10,2,2)
+        number_panel = curses.panel.new_panel(number_window)
+        count = COUNT
+        while 1:
+            if RUN_UPDATE_WINDOW_THREAD:
+                sleep(0.001)
+                if count != COUNT:
+                    count = COUNT
+                    number_panel.top()
+                    number_window.addstr(1,1,str(count))
+                    number_window.refresh()
+                    curses.panel.update_panels()
+            else:
+                return
+
+
+def change_count(value):
+    global COUNT
+    COUNT += value
+
+
+def number_updater():
+    while 1:
+        if RUN_UPDATE_NUMBER_THREAD:
+            sleep(0.001)
+            change_count(COUNT_PER_MILLISECOND)
+        else:
+            return
+
+def run_update_window_thread(arg):
+    global RUN_UPDATE_WINDOW_THREAD
+    if arg:
+        RUN_UPDATE_WINDOW_THREAD = True
+    else:
+        RUN_UPDATE_WINDOW_THREAD = False
+
+
+def run_update_number_thread(arg):
+    global RUN_UPDATE_NUMBER_THREAD
+    if arg:
+        RUN_UPDATE_NUMBER_THREAD = True
+    else:
+        RUN_UPDATE_NUMBER_THREAD = False
+
+
+def main(stdscr):
+    curses.initscr()
+    curses.start_color()
+    curses.use_default_colors()
+    curses.cbreak()  # No need for [Return]
+    curses.noecho()  # Stop keys being printed
+    curses.curs_set(0)  # Invisible cursor
+    stdscr.keypad(True)
+    stdscr.clear()
+    curses.panel.update_panels()
+    
+    run_update_window_thread(True)
+    update_window_thread = Thread(target = number_window_updater)
+    update_window_thread.start()
+
+    run_update_number_thread(True)
+    update_number_thread = Thread(target = number_updater)
+    update_number_thread.start()
+
+    stdscr.addstr(5,10,"Press Q to exit.")
+
+    while True:
+        key = stdscr.getkey()
+        if key == "q":
+            run_update_window_thread(False)
+            run_update_number_thread(False)
+            sleep(0.01)
+            quit()
+        elif key == " ":
+            change_count(10000)
+
+
+
+
+
+curses.wrapper(main)
